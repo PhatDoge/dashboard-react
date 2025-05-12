334986dd2f22dfa38e36109f0a52aec5f4f0136f
PhatDoge <alonso.castillogon@gmail.com>
2025-05-11 22:04:16 -0400

    Refactor routing structure and add admin layout components

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+const Adminlayout = () => {
+  return (
+    <div className="admin-layout">
+      Mobile Sidebar
+      <aside className="w-full max-w-[270px] hidden lg:block"></aside>
+      <aside className="children">
+        <Outlet />
+      </aside>
+    </div>
+  );
+};
+
+export default Adminlayout;
