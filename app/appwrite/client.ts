d8363a9be6ac7d7c8fba588d8dfa9696afe820f0
PhatDoge <alonso.castillogon@gmail.com>
2025-05-14 18:48:16 -0400

    Add Appwrite configuration to client.ts

---
+++ app/appwrite/client.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+  endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  apikey: import.meta.env.VITE_APPWRITE_API_KEY,
+  databseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  tripCollectionId: import.meta.env.VITE_APPWRITE_TRIP_COLLECTION_ID,
+};
