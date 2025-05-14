1da05b03f071b98051eb06f8a2259ea67d2fb20d
PhatDoge <alonso.castillogon@gmail.com>
2025-05-14 19:39:01 -0400

    Refactor Appwrite configuration and implement user authentication functions

--- app/appwrite/client.ts
+++ app/appwrite/client.ts
@@ -1,11 +1,22 @@
+import { Account, Client, Databases, Storage } from "appwrite";
+
 // appwrite config
 export const appwriteConfig = {
   endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   apikey: import.meta.env.VITE_APPWRITE_API_KEY,
-  databseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
-  tripCollectionId: import.meta.env.VITE_APPWRITE_TRIP_COLLECTION_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+  tripCollectionId: import.meta.env.VITE_APPWRITE_TRIPS_COLLECTION_ID,
 };
 
-//APPWRITE SETUP
+//APPWRITE SETUP(
+const client = new Client()
+  .setEndpoint(appwriteConfig.endpointUrl)
+  .setProject(appwriteConfig.projectId);
+
+const account = new Account(client);
+const database = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, database, storage };
